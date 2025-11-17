cf67d95052dd152dda0f9b8990b845fb6418bda2
SkyZYNE <raffaelgunawan19@gmail.com>
2025-11-17 15:30:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab_week_11_a/PreferenceViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.lab_week_11_a
+
+class PreferenceViewModel(private val preferenceWrapper: PreferenceWrapper) : ViewModel() {
+    // Save the text to the shared preferences
+    fun saveText(text: String) {
+        preferenceWrapper.saveText(text)
+    }
+    // Get the text from the shared preferences
+    fun getText(): LiveData<String> {
+        return preferenceWrapper.getText()
+    }
+}
